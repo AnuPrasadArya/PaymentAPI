83f9dd01c4b4e79e817f20581cfcc2a09ce88a65
AnuPrasad <tradeanuprasad@gmail.com>
2025-06-20 01:24:25 +0530

    Card Validation APIChanges

---
+++ PaymentAPI/Application/Interfaces/ICardValidation.cs
@@ -0,0 +1,7 @@
+﻿namespace PaymentAPI.Application.Interfaces
+{
+    public interface ICardValidation
+    {
+        Task<bool> ValidateCard(string CardNumber, int ExpiryMonth, int ExpiryYear);
+    }
+}
